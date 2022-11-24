a681885eb67a77192db3701b639f90efe14f0f95
Joakim Jönsson <jockejockejockejocke@gmail.com>
2022-11-24 20:00:32 +0100

    Updated UI.

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const inputRoute = document.querySelector('.route__input');
 const routeBtn = document.querySelector('.route__btn');
 const departureElem = document.querySelector('.departure');
 const arrivalElem = document.querySelector('.arrival');
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
 
 routeBtn.addEventListener('click', getRoute);
 
@@ -16,12 +22,20 @@ function getRoute() {
 function getDay(choosenDay, choosenRoute) {
   let d = new Date(choosenDay);
   // d.setDate(d.getDate() + choosenDay);
-  departureElem.textContent = `${d}`;
-  console.log(`Avgångsdag är: ${d}day`);
+  departureElem.textContent = `${d.toLocaleDateString(
+    'en-US',
+    options
+  )}`;
+  console.log(
+    `Avgångsdag är: ${d.toLocaleDateString('en-US', options)}day`
+  );
 
   d.setDate(d.getDate() + choosenRoute);
   console.log(`Ankomstdagen är: ${d}`);
-  arrivalElem.textContent = `${d}`;
+  arrivalElem.textContent = `${d.toLocaleDateString(
+    'en-US',
+    options
+  )}`;
 }
 
 function removeResults() {
